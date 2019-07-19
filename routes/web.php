4af68eb8da60c53755fa2e39cd149fdc36591737
EC2 Default User <ec2-user@ip-172-31-16-182.us-east-2.compute.internal>
2019-07-19 07:37:10 +0000

    commit2

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/news/create','Admin/NewsController@add');
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/update','Admin\NewsController@update');
+    Route::get('news/delete','Admin\NewsController@delete');
+    
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
+
